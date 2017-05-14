09480b226e366b3dc390e490bfe98c383ea95360
kevinwei30 <kevinwei840223@gmail.com>
2017-05-14 17:43:12 +0800

    update customer kmeans

--- src/customer_kmeans.py
+++ src/customer_kmeans.py
@@ -2,135 +2,155 @@ import tensorflow as tf
 import csv
 import random
 import numpy as np
+import pickle
 import json
+from utils import *
+import os
 
 gpu_options = tf.GPUOptions(per_process_gpu_memory_fraction=0.1)
 sess = tf.Session(config=tf.ConfigProto(gpu_options=gpu_options))
 
-# data features
-customer_data = []
-with open('../data/customers.csv', 'r') as f:
-	data = csv.reader(f)
-	for d in data:
-		customer_data.append(d)
+root_dir = './'
+data_dir = os.path.join(root_dir, 'data')
 
-customer_data = customer_data[1:]
+userFile = os.path.join(data_dir, 'user_data.pkl')
+clusterFile = os.path.join(data_dir, 'user_clusters.pkl')
 
-print len(customer_data)
+def createCustomerData():
+	# data features
+	customer_data = []
+	with open('./data/customers.csv', 'r') as f:
+		data = csv.reader(f)
+		for d in data:
+			customer_data.append(d)
 
-for c in customer_data:
-	#c[0] = int(c[0])
-	if c[1] == 'F':
-		c[1] = 0.
-	elif c[1] == 'M':
-		c[1] = 1.
-	for j in range(2, 10):
-		if c[j] == 'N':
-			c[j] = 0.
-		elif c[j] == 'Y':
-			c[j] = 1.
-	c[10] = float(c[10])
-	c[11] = float(c[11])
-	# id = index+1
-	c.pop(0)
+	customer_data = customer_data[1:]
 
-data_vectors = customer_data
-init_n = 50000
-init_users = data_vectors[:init_n]
-follow_users = data_vectors[init_n:]
+	print len(customer_data)
 
-# data structure
-num_prod_clusters = 20
+	for c in customer_data:
+		#c[0] = int(c[0])
+		if c[1] == 'F':
+			c[1] = 0.
+		elif c[1] == 'M':
+			c[1] = 1.
+		for j in range(2, 10):
+			if c[j] == 'N':
+				c[j] = 0.
+			elif c[j] == 'Y':
+				c[j] = 1.
+		c[10] = float(c[10])
+		c[11] = float(c[11])
+		# id = index+1
+		c.pop(0)
 
-num_vectors = 200000
-num_clusters = 20
-num_steps = 100
-num_dims = 11
+	data_vectors = customer_data
+	init_n = 50000
+	init_users = data_vectors[:init_n]
+	follow_users = data_vectors[init_n:]
 
-user_data = []
-for i in range(num_vectors):
-	user_tmp = dict()
-	user_tmp['user_id'] = i
-	user_tmp['features'] = data_vectors[i]
-	if (i < init_n):
-		user_tmp['scores'] = [round(random.uniform(0, 10), 3) for j in range(num_prod_clusters)]
-	else:
-		user_tmp['scores'] = [0. for j in range(num_prod_clusters)]
-	user_tmp['cluster_id'] = -1
-	user_data.append(user_tmp)
+	# data structure
+	num_prod_clusters = 20
 
-user_clusters = []
-for i in range(num_clusters):
-	cluster_tmp = dict()
-	cluster_tmp['cluster_id'] = i
-	cluster_tmp['user_ids'] = []
-	cluster_tmp['scores'] = [0. for j in range(num_prod_clusters)]
-	user_clusters.append(cluster_tmp)
+	num_vectors = 200000
+	num_clusters = 18
+	num_steps = 100
+	num_dims = 11
 
-# k-means
-#vectors = tf.constant(init_users)
-#random_indices = tf.random_shuffle(tf.range(0, num_vectors))
-#centroid_indices = tf.slice(random_indices, [0,], [num_clusters,])
-#init_centroids = tf.Variable(tf.gather(data_vectors, centroid_indices))
-init_centroids = tf.Variable(init_users[:num_clusters])
+	user_data = []
+	for i in range(num_vectors):
+		user_tmp = dict()
+		user_tmp['user_id'] = i
+		user_tmp['features'] = data_vectors[i]
+		if (i < init_n):
+			user_tmp['scores'] = [round(random.uniform(0, 10), 3) for j in range(num_prod_clusters)]
+		else:
+			user_tmp['scores'] = [0. for j in range(num_prod_clusters)]
+		user_tmp['cluster_id'] = -1
+		user_data.append(user_tmp)
 
-centroids = tf.placeholder('float32', [num_clusters, num_dims])
-vectors = tf.placeholder('float32', [None, num_dims])
+	user_clusters = []
+	for i in range(num_clusters):
+		cluster_tmp = dict()
+		cluster_tmp['cluster_id'] = i
+		cluster_tmp['user_ids'] = []
+		cluster_tmp['scores'] = [0. for j in range(num_prod_clusters)]
+		user_clusters.append(cluster_tmp)
 
-expanded_vectors = tf.expand_dims(vectors, 0)
-expanded_centroids = tf.expand_dims(centroids, 1)
+	# k-means
+	#vectors = tf.constant(init_users)
+	#random_indices = tf.random_shuffle(tf.range(0, num_vectors))
+	#centroid_indices = tf.slice(random_indices, [0,], [num_clusters,])
+	#init_centroids = tf.Variable(tf.gather(data_vectors, centroid_indices))
+	init_centroids = tf.Variable(init_users[:11] + init_users[12:19])
 
-vectors_sub = tf.subtract(expanded_vectors, expanded_centroids)
-distances = tf.reduce_sum(tf.square(vectors_sub), 2)
-assignments = tf.to_int32(tf.argmin(distances, 0))
+	centroids = tf.placeholder('float32', [num_clusters, num_dims])
+	vectors = tf.placeholder('float32', [None, num_dims])
 
-partitions = tf.dynamic_partition(vectors, assignments, num_clusters)
-update_centroids = tf.concat([tf.expand_dims(tf.reduce_mean(partition, 0), 0) for partition in partitions], 0)
+	expanded_vectors = tf.expand_dims(vectors, 0)
+	expanded_centroids = tf.expand_dims(centroids, 1)
 
-# session
-init = tf.initialize_all_variables()
-sess = tf.Session()
-sess.run(init)
+	vectors_sub = tf.subtract(expanded_vectors, expanded_centroids)
+	distances = tf.reduce_sum(tf.square(vectors_sub), 2)
+	assignments = tf.to_int32(tf.argmin(distances, 0))
 
-new_centroids = sess.run(init_centroids)
-for step in xrange(num_steps):
-	print step
-	#tmp = sess.run(update_centroids)
-	#print tmp
-	new_centroids, assignment_values = sess.run([update_centroids, assignments], feed_dict={centroids: new_centroids, vectors: init_users})
-	#print new_centroids
-	#print len(assignment_values)
+	partitions = tf.dynamic_partition(vectors, assignments, num_clusters)
+	update_centroids = tf.concat([tf.expand_dims(tf.reduce_mean(partition, 0), 0) for partition in partitions], 0)
 
-# updata data
-current_centroids = new_centroids
+	# session
+	init = tf.initialize_all_variables()
+	sess = tf.Session()
+	sess.run(init)
 
-for i in range(len(assignment_values)):
-	user_data[i]['cluster_id'] = assignment_values[i]
-	user_clusters[assignment_values[i]]['user_ids'].append(i)
+	new_centroids = sess.run(init_centroids)
+	for step in xrange(num_steps):
+		print step
+		new_centroids, assignment_values = sess.run([update_centroids, assignments], feed_dict={centroids: new_centroids, vectors: init_users})
 
-for i in range(len(user_clusters)):
-	for j in user_clusters[i]['user_ids']:
-		user_clusters[i]['scores'] = np.add(user_clusters[i]['scores'], user_data[j]['scores'])
+	# updata data
+	current_centroids = new_centroids
 
-new_centroids, assignment_values = sess.run([update_centroids, assignments], feed_dict={centroids: current_centroids, vectors: follow_users})
-#print len(assignment_values)
+	for i in range(len(assignment_values)):
+		user_data[i]['cluster_id'] = assignment_values[i]
+		user_clusters[assignment_values[i]]['user_ids'].append(i)
 
-for i in range(len(assignment_values)):
-	user_data[init_n + i]['cluster_id'] = assignment_values[i]
-	user_clusters[assignment_values[i]]['user_ids'].append(init_n + i)
+	for i in range(len(user_clusters)):
+		user_clusters[i]['centroid'] = new_centroids[i].tolist()
+		for j in user_clusters[i]['user_ids']:
+			user_clusters[i]['scores'] = np.add(user_clusters[i]['scores'], user_data[j]['scores'])
+		user_clusters[i]['scores'] = np.array(user_clusters[i]['scores']).tolist()
 
-#print user_data[100000]
-for i in range(num_clusters):
-	print user_clusters[i]['scores'], len(user_clusters[i]['user_ids'])
+	new_centroids, assignment_values = sess.run([update_centroids, assignments], feed_dict={centroids: current_centroids, vectors: follow_users})
 
-with open('user_data.txt', 'w') as f_user:
-	#json.dumps(user_data, f_user)
-	for user in user_data:
-		f_user.write(str(user))
+	for i in range(len(assignment_values)):
+		user_data[init_n + i]['cluster_id'] = assignment_values[i]
+		user_clusters[assignment_values[i]]['user_ids'].append(init_n + i)
 
-with open('user_clusters.txt', 'w') as f_cluster:
-	#json.dumps(user_clusters, f_cluster)
-	for cluster in user_clusters:
-		f_cluster.write(str(cluster))
+	#print user_data[100000]
+	for i in range(num_clusters):
+		print user_clusters[i]['scores'], len(user_clusters[i]['user_ids'])
 
+	with open('user_data.pkl', 'w') as f_user:
+		pickle.dump(user_data, f_user)
+		#json.dump(user_data, f_user)
+		#for user in user_data:
+		#	f_user.write(str(user))
 
+	with open('user_clusters.pkl', 'w') as f_cluster:
+		pickle.dump(user_clusters, f_cluster)
+		#json.dump(user_clusters, f_cluster)
+		#for cluster in user_clusters:
+		#	f_cluster.write(str(cluster))
+
+def getUserData():
+	if (not os.path.exist(userFile)):
+		createCustomerData()
+	return readPickle(userFile)
+
+def getUserClusterData():
+	if (not os.path.exist(clusterFile)):
+		createCustomerData()
+	return readPickle(clusterFile)
+
+if __name__ == '__main__':
+	createCustomerData()
