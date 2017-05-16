8e717ce36e58583df8dd7bff7d8515f17da146f6
Bob Liang <bob831009@gmail.com>
2017-05-16 20:37:47 +0800

    add name to fc_info

--- src/create_financial_commissioner.py
+++ src/create_financial_commissioner.py
@@ -5,6 +5,12 @@ CATEGORY_NUM = 20;
 COMMISSIONER_NUM = 20;
 TOP_NUM = 5;
 output_fileName = '../data/financial_commissioner.json'
+total_name = [];
+with open('../data/nameList.txt') as f:
+	for line in f:
+		line = line.strip().split(' ');
+		name = line[0];
+		total_name.append(name);
 
 res = [];
 for i in range(COMMISSIONER_NUM):
@@ -12,6 +18,7 @@ for i in range(COMMISSIONER_NUM):
 	tmp_top5 = random.sample(range(CATEGORY_NUM), TOP_NUM);
 	commissioner_info['top5'] = tmp_top5;
 	commissioner_info['id'] = i;
+	commissioner_info['name'] = total_name[i];
 	commissioner_info['profile_path'] = '../data/financial_commissioner_image/%d.jpg'%(i);
 	commissioner_info['seniority'] = 5;
 	res.append(commissioner_info);
